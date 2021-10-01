478ddcffae968408cb513c5796a3019b53dfe791
Leia Renée <leiarenee20@gmail.com>
2021-10-01 11:05:54 +0200

    Update build-docker.sh

--- build-docker.sh
+++ build-docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker build . -t tmnl-api --build-arg PYTHON_VERSION=3.9.5 --build-arg PYTHON_COMMAND=3.9
+docker build . -t smpl-api --build-arg PYTHON_VERSION=3.9.5 --build-arg PYTHON_COMMAND=3.9
